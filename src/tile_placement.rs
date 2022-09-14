cc5904075fdb061824a9153b217f1241e739aed3
SecretPocketCat <jiridexterhanc@gmail.com>
2022-09-14 08:30:03 +0200

    tweak board proto

--- src/tile_placement.rs
+++ src/tile_placement.rs
@@ -1,6 +1,6 @@
 use crate::{mouse::CursorWorldPosition, GameState};
 use bevy::prelude::*;
-use bevy_prototype_lyon::prelude::*;
+use bevy_prototype_lyon::prelude::{tess::geom::euclid::num::Floor, *};
 use iyes_loopless::prelude::*;
 
 pub struct TilePlacementPlugin;
@@ -19,27 +19,46 @@ fn setup_board(mut cmd: Commands) {
         extents: Vec2::splat(size),
         ..shapes::Rectangle::default()
     };
-    let mut builder = GeometryBuilder::new().add(&square);
+    let builder = GeometryBuilder::new().add(&square);
 
-    let section_count = 4;
+    cmd.spawn_bundle(builder.build(
+        DrawMode::Fill(FillMode::color(Color::ANTIQUE_WHITE)),
+        Transform::default(),
+    ));
+
+    let mut builder = GeometryBuilder::new();
+
+    let section_count = 9;
     let section_count_half = section_count / 2;
     let section_size = size / section_count as f32;
     for i in -section_count_half..section_count_half {
-        let x = section_size * i as f32;
+        let x = section_size * i as f32 + section_size / 2.;
         let line_x = shapes::Line(Vec2::new(x, extents), Vec2::new(x, -extents));
         let line_y = shapes::Line(Vec2::new(extents, x), Vec2::new(-extents, x));
         builder = builder.add(&line_x).add(&line_y);
     }
 
     cmd.spawn_bundle(builder.build(
-        DrawMode::Outlined {
-            fill_mode: FillMode::color(Color::ANTIQUE_WHITE),
-            outline_mode: StrokeMode::new(Color::DARK_GRAY, 10.0),
-        },
+        DrawMode::Stroke(StrokeMode::new(Color::GRAY, 5.0)),
         Transform::default(),
     ));
 
-    info!("board setup");
+    let mut builder = GeometryBuilder::new().add(&square);
+
+    let section_count = 3;
+    let section_count_half = section_count / 2;
+    let section_size = size / section_count as f32;
+    for i in -section_count_half..section_count_half {
+        let x = section_size * i as f32 + section_size / 2.;
+        let line_x = shapes::Line(Vec2::new(x, extents), Vec2::new(x, -extents));
+        let line_y = shapes::Line(Vec2::new(extents, x), Vec2::new(-extents, x));
+        builder = builder.add(&line_x).add(&line_y);
+    }
+
+    cmd.spawn_bundle(builder.build(
+        DrawMode::Stroke(StrokeMode::new(Color::DARK_GRAY, 12.0)),
+        Transform::default(),
+    ));
 }
 
 fn update_tile_coords(cursor_pos: Res<CursorWorldPosition>) {
